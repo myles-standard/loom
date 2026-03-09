4f66a08c009b3415f13c8c159cb0e40ca33de355
myles-standard <myles.es@outlook.com>
2026-03-08 22:35:02 -0400

    Implements the initial Banner configuration.

---
+++ frontend/src/components/Banner.tsx
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Banner() {
+    return (
+        <Navbar expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Media Magic</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Options" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#option1">Option 1</NavDropdown.Item>
+                            <NavDropdown.Item href="#option2">Option 2</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Banner;
