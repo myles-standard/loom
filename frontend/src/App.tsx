e4f87ad717a02b91497bbb4b7d8d783e164b642c
myles-standard <myles.es@outlook.com>
2026-03-10 22:11:57 -0400

    Adds boostrap container and row classes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Banner from './components/Banner';
 import ConversionOptions from './components/ConversionOptions';
 import FileInfo from './components/FileInfo';
 import UploadFrom from './components/UploadForm';
 import FilePreview from './components/FilePreview';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
 
   return (
-    <div>
+    <>
       <Banner />
-      <UploadFrom setFile={setFile} />
-      {file && (
+      <Container>
+        <UploadFrom file={file} setFile={setFile} />
+        {file && (
+          <Row className="mt-4">
             <FilePreview file={file} />
-          <FileInfo file={file} />
-          <ConversionOptions file={file} />
-        </>
-      )}
-    </div>
+            <FileInfo file={file} />
+            <ConversionOptions file={file} />
+          </Row>
+        )}
+      </Container>
+    </>
   )
 }
 
